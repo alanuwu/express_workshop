793b0f60a198056b1b83a7ba3e6976c4ce62cf51
Alan Eduardo Barrera Gudiño <alanbg772@gmail.com>
2024-09-14 22:53:26 -0600

    Se agregaron las modificaciones de la actividad 5

--- index.js
+++ index.js
@@ -2,39 +2,45 @@ const express = require('express');
 const app = express();
 const { pokemon } = require('./pokedex.json');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res, next) => {
-	res.status(200);
-	res.send("Bienvenido a la pokedex");
+	return res.status(200).send("Bienvenido a la pokedex");
 });
 
+app.post("/pokemon", (req, res, next) => {
+	return res.status(200).send(req.body.name);
+})
+
 app.get("/pokemon/all", (req, res, next) => {
-	res.status(200);
-	res.send(pokemon);
+	return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
-	const id = req.params.id -1;
-	if(pokemon[id]){
-	res.status(200);
-	res.send(pokemon[id]);
-	}else{
-		res.status(404);
-		res.send("Pokemon no encontrado");
+	const id = req.params.id - 1;
+	if (pokemon[id]) {
+		return res.status(200).send(pokemon[id]);
 	}
-	
-}); 
+	return res.status(404).send("Pokemon no encontrado");
+});
 
-app.get("/pokemon/:name", (req, res, next) => {
+app.get("/pokemon/:name([A-zA-z]+)", (req, res, next) => {
 	const name = req.params.name;
-	for( i = 0; i < pokemon.length; i++){
-		if(pokemon[i].name == name){
-			res.status(200);
-			res.send(pokemon[i]);
-		}
+
+	const pk = pokemon.filter((p) => {
+		return p.name.toUpperCase() == name.toUpperCase() ? p : null;
+	});
+	console.log(pk);
+
+
+	if(pk.length > 0 ){
+		return res.status(200).send(pk);
 	}
-	res.status(404);
-	res.send('Pokemon no encontrado');
+		else{
+			return res.status(404).send('Pokemon no encontrado');
+		}
+		
 });
 
 app.listen(process.env.PORT || 3000, () => {
