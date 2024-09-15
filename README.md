2b1171e08a990602839fe712a847df705724be0b
Alan Eduardo Barrera Gudiño <alanbg772@gmail.com>
2024-09-14 21:47:17 -0600

    cambios_actividad4

---
+++ README.md
@@ -0,0 +1,2 @@
+Recopilacion de archivos requeridos para completar los desafíos de javascripting, tambien contiene programas hechos
+en el curso de desarrollo de una API en nodeJS.
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json');
 
 
 app.get("/", (req, res, next) => {
 	res.status(200);
-	res.send("Bienvenido");
+	res.send("Bienvenido a la pokedex");
 });
-app.listen(3000, ()  => {
+
+app.get("/pokemon/all", (req, res, next) => {
+	res.status(200);
+	res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
+	const id = req.params.id -1;
+	if(pokemon[id]){
+	res.status(200);
+	res.send(pokemon[id]);
+	}else{
+		res.status(404);
+		res.send("Pokemon no encontrado");
+	}
+	
+}); 
+
+app.get("/pokemon/:name", (req, res, next) => {
+	const name = req.params.name;
+	for( i = 0; i < pokemon.length; i++){
+		if(pokemon[i].name == name){
+			res.status(200);
+			res.send(pokemon[i]);
+		}
+	}
+	res.status(404);
+	res.send('Pokemon no encontrado');
+});
+
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is running...");
 });
