52d8d3a16393c5f3b78766f2c77dbaad32c51863
Alan Eduardo Barrera Gudiño <alanbg772@gmail.com>
2024-10-12 23:17:09 -0600

    Clase 12. Implementacion de JWT

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+//Dependencias
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+//Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
+//Middleware
+const auth = require("./middleware/auth");
+const notFound = require("./middleware/notFound");
+const index = require("./middleware/index");
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -10,18 +16,15 @@ app.use(express.urlencoded({extended: true}));
 
 
 
-app.get("/", (req, res, next) => {
-	return res.status(200).json({code: 1, message: "Bienvenido a la pokedex"});
-});
+app.get("/",index);
+
+app.use("/user", user);
+app.use(auth);
 
 app.use('/pokemon', pokemon);
 
-app.use("/user",user);
 
-app.use((req, res, next) => {
-	return res.status(404).json({code: 404, message: "URL no encontrada"});
-
-})
+app.use(notFound);
 
 
 app.listen(process.env.PORT || 3000, () => {
